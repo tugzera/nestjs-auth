6ee434a58a21a5fd412ae6878430266cd043b0c1
tugzera <damascenobdm@gmail.com>
2021-11-15 17:17:50 -0300

    feat: add jwt libs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
 
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { CaslModule } from './casl/casl.module';
+import { JwtStrategy } from './jwt-strategy';
 
 @Module({
+  imports: [
+    CaslModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '15m' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [CaslModule],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
