aa94c1743b0d39544b0ab22082bc1c104a6efb2f
tugzera <damascenobdm@gmail.com>
2021-11-15 08:56:31 -0300

    feat: test casl implementation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { CaslModule } from './casl/casl.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [CaslModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Action } from './casl/action-enum';
+import { Article } from './casl/article';
+import { CaslAbilityFactory } from './casl/casl-ability.factory';
+import { User } from './casl/user';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private caslAbilityFactory: CaslAbilityFactory) {}
+
+  async test() {
+    const user = new User();
+    user.id = 1;
+    user.isAdmin = false;
+
+    const article = new Article();
+    article.authorId = 2;
+
+    const ability = this.caslAbilityFactory.createForUser(user);
+    console.log(ability);
+    console.log(ability.can(Action.Read, article)); // true
+    console.log(ability.can(Action.Delete, article)); // false
+    console.log(ability.can(Action.Create, article)); // false
+    console.log(ability.can(Action.Update, article)); // true
+  }
+}
